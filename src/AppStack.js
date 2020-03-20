38f51a1524b7b94279ead88a72f5da2d153c31b8
Omer Quadri M <omar@DSmart.com>
2020-03-20 17:45:41 +0530

    Adding Deep linking

--- src/AppStack.js
+++ src/AppStack.js
@@ -1,6 +1,6 @@
 import {createStackNavigator} from 'react-navigation-stack';
 import {createAppContainer} from 'react-navigation';
-
+import React from 'react';
 import MainPage from './MainPage';
 import TokScreen from './TokBoxScreen';
 
@@ -11,12 +11,14 @@ const AppStack = createStackNavigator(
       navigationOptions: {
         headerShown: false,
       },
+      path: 'mainpage',
     },
     TokScreen: {
       screen: TokScreen,
       navigationOptions: {
         headerShown: false,
       },
+      path: 'callschedule',
     },
   },
   {
@@ -24,4 +26,9 @@ const AppStack = createStackNavigator(
   },
 );
 
-export default createAppContainer(AppStack);
+const AppContainer = createAppContainer(AppStack);
+
+export default () => {
+  const prefix = 'tokbox://';
+  return <AppContainer uriPrefix={prefix} />;
+};
